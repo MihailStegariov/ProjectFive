7234b898a049fce5d7bd2a5ed2f50a4a4b7ea7bc
Mihail <Mihail@LAPTOP-DTKS6POR.fios-router.home>
2021-11-22 22:00:08 -0500

    Commit Five....

---
+++ src/five/Five.java
@@ -0,0 +1,14 @@
+package five;
+
+public class Five {
+
+	public static void main(String[] args) {
+		
+		int i = 0; 
+		int b = 9;
+		
+		int result = i+b;
+		
+		System.out.println(result);
+	}
+}
